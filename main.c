de70c7435cb4243c607f10bb719a43364b35f284
d601 <shmeep@gmail.com>
2016-02-06 15:12:06 -0800

    Error check initialization of the stupid_engine

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main(int argc, char *argv[])
 {
     struct stupid_engine *engine;
-    stupid_engine_new(engine);
+    if(stupid_engine_new(engine) < 0)
+        return -1;
+
     return stupid_engine_start(engine);
 }
